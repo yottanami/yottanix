65630eae531b91def08615f78724c192e58b2e81
yottanami <yottanami@gnu.org>
2025-10-17 15:49:31 +0200

    Fix bluetooth headphone issue

--- configuration.nix
+++ configuration.nix
@@ -1,13 +1,26 @@
 { config, pkgs, lib, inputs, ... }:
 
-{
+let
+  xautolockLocker = pkgs.writeShellScript "xautolock-locker" ''
+    exec ${pkgs.lightdm}/bin/dm-tool lock
+  '';
+
+  xautolockCleanup = pkgs.writeShellScript "xautolock-cleanup" ''
+    ${pkgs.procps}/bin/pkill xautolock || true
+  '';
+
+  bluezWithCodecs = pkgs.bluezFull or pkgs.bluez;
+in {
   imports = [
     ./hardware-configuration.nix
     ./noteditor.nix
-    inputs.sops-nix.nixosModules.sops 
+    inputs.sops-nix.nixosModules.sops
   ];
 
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot = {
+    enable = true;
+    configurationLimit = 5;
+  };
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "yottawork";
@@ -20,51 +33,53 @@
 
   services.xserver.enable = true;
   services.xserver.windowManager.noteditor.enable = true;
-  services.xserver.windowManager.exwm.enable = true;  
+  services.xserver.windowManager.exwm.enable = true;
   services.xserver.xkb.layout = "us,ir";
-  ##  services.xserver.xkb.options = "ctrl:swapcaps,grp:alt_caps_toggle";
   services.xserver.xkb.options = "grp:alt_caps_toggle";
 
   systemd.user.services.xautolock = {
     description = "Start xautolock with cleanup";
     after = [ "graphical-session.target" ];
+    wantedBy = [ "default.target" ];
     serviceConfig = {
       Type = "simple";
-      ExecStartPre = "${pkgs.procps}/bin/pkill xautolock || true";
-      ExecStart = "${pkgs.xautolock}/bin/xautolock -time 10 -locker 'dm-tool lock'";
+      ExecStartPre = xautolockCleanup;
+      ExecStart = [
+        "${pkgs.xautolock}/bin/xautolock"
+        "-time" "10"
+        "-locker" xautolockLocker
+      ];
       Restart = "always";
     };
-    wantedBy = [ "default.target" ];
   };
 
-  # Provides a standard interface for devices like touchpads, mice, and keyboards
   services.libinput.enable = true;
-  
-  # Allows unprivileged users to set their processes to run with real-time priority, which is necessary for pipewire
-  security.rtkit.enable   = true;
+  security.rtkit.enable = true;
 
   services.pipewire = {
-    enable          = true;
-    jack.enable     = true;
-    alsa.enable     = true;
-    alsa.support32Bit = true;
-    pulse.enable    = true;
+    enable = true;
+    wireplumber.enable = true;
+    pulse.enable = true;
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
+    jack.enable = true;
   };
 
   users.groups.realtime = {};
 
   virtualisation.docker.enable = true;
   virtualisation.docker.rootless = {
-    enable           = true;
+    enable = true;
     setSocketVariable = true;
   };
 
   services.printing.enable = true;
 
-  # Enable Avahi service for network discovery
   services.avahi = {
-    enable       = true;
-    nssmdns4     = true;
+    enable = true;
+    nssmdns4 = true;
     openFirewall = true;
   };
 
@@ -77,9 +92,9 @@
 
   users.users.yottanami = {
     isNormalUser = true;
-    shell        = pkgs.fish;
-    extraGroups  = [ "wheel" "audio" "realtime" "networkmanager" "docker" ];
-    packages     = with pkgs; [
+    shell = pkgs.fish;
+    extraGroups = [ "wheel" "audio" "realtime" "networkmanager" "docker" ];
+    packages = with pkgs; [
       graphviz
       plantuml
       vlc
@@ -121,7 +136,7 @@
       pre-commit
       postman
       aider-chat-full
-      spotify 
+      spotify
     ];
   };
 
@@ -133,7 +148,7 @@
   environment.sessionVariables = {
     NOTEDITOR_WM_PATH = "/home/yottanami/src/personal/noteditor/";
   };
-  
+
   environment.systemPackages = with pkgs; [
     wget
     emacs
@@ -163,10 +178,9 @@
   ];
 
   programs.udevil.enable = true;
-  programs.fish.enable   = true;
+  programs.fish.enable = true;
   programs.nix-ld.enable = true;
-
-  programs.java = { enable = true; };
+  programs.java.enable = true;
 
   services.udev.packages = [ pkgs.via pkgs.teensy-udev-rules ];
 
@@ -174,60 +188,71 @@
     (final: prev: {
       udevil = prev.udevil.overrideAttrs (_: {
         postInstall = ''
-          sed -i 's/^allowed_types =.*/allowed_types = $KNWON_FILESYSTEMS, file, cifs, nfs, curlftpfs, ftpfs, sshfs, davfs/' \
+          sed -i 's/^allowed_types =.*/allowed_types = $KNOWN_FILESYSTEMS, file, cifs, nfs, curlftpfs, ftpfs, sshfs, davfs/' \
             $out/etc/udevil/udevil.conf
         '';
       });
     })
   ];
 
-  services.logind.settings = {
-    Login = {
-      HandleLidSwitch = "suspend-then-hibernate";
-      HandleLidSwitchExternalPower = "suspend-then-hibernate";
-      HandleLidSwitchDocked = "ignore";
-      LidSwitchIgnoreInhibited = "yes";
-    };
+  services.logind = {
+    lidSwitch = "suspend-then-hibernate";
+    lidSwitchExternalPower = "suspend-then-hibernate";
+    lidSwitchDocked = "ignore";
+    extraConfig = ''
+      LidSwitchIgnoreInhibited=yes
+    '';
   };
 
-  services.hardware.bolt.enable  = true;
-  hardware.bluetooth.enable      = true;
-  hardware.bluetooth.powerOnBoot = true;
-  services.blueman.enable        = true;
+  services.hardware.bolt.enable = true;
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    package = bluezWithCodecs;
+  };
+  services.blueman.enable = true;
 
-  ## REMOVE ME
-  # hardware.enableAllFirmware = true;
-  # hardware.enableRedistributableFirmware = true;
-  # nixpkgs.config.allowUnfree = true;
+  services.pipewire.wireplumber.configPackages = [
+    (pkgs.writeTextDir "share/wireplumber/bluetooth.lua.d/51-high-quality.lua" ''
+      bluez_monitor.properties = {
+        ["bluez5.enable-sbc-xq"] = true;
+        ["bluez5.enable-msbc"] = true;
+        ["bluez5.enable-hw-volume"] = true;
+        ["bluez5.default.profile"] = "a2dp-sink";
+        ["bluez5.codecs"] = {
+          "ldac", "aptx", "aptx_hd", "aac", "sbc_xq", "sbc"
+        };
+      }
+    '')
+  ];
+
+  hardware.enableAllFirmware = true;
+  hardware.enableRedistributableFirmware = true;
+  nixpkgs.config.allowUnfree = true;
 
   systemd.user.services.mpris-proxy = {
-    description   = "Mpris proxy";
-    after         = [ "network.target" "sound.target" ];
-    wantedBy      = [ "default.target" ];
+    description = "Mpris proxy";
+    after = [ "network.target" "sound.target" ];
+    wantedBy = [ "default.target" ];
     serviceConfig.ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
   };
 
   system.autoUpgrade = {
-    enable  = true;
-    flake   = inputs.self.outPath;
-    flags   = [ "--update-input" "nixpkgs" "-L" ];
-    dates   = "02:00";
+    enable = true;
+    flake = inputs.self.outPath;
+    flags = [ "--update-input" "nixpkgs" "-L" ];
+    dates = "02:00";
     randomizedDelaySec = "45min";
   };
 
-
-  ########################################
-  ##  SOPS‑NIX: secrets → env‑file
-  ########################################
-
+  ## Uncomment once the secret file is staged.
   # sops = {
   #   defaultSopsFile = ./secrets/openrouter.yaml;
-  #   age.keyFile     = "/var/keys/host.agekey";
+  #   age.keyFile = "/var/keys/host.agekey";
   #   secrets.OPENROUTER_API_KEY = { };
   # };
-
-  # # export to every new login shell
   # environment.sessionVariables.OPENROUTER_API_KEY =
   #   config.sops.secrets.OPENROUTER_API_KEY;
-  system.stateVersion = "24.14";
+
+  system.stateVersion = "24.05";
 }
